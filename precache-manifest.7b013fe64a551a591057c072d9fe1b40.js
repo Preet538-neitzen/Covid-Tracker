2363072d65a6cbf5757fcbc011c23695ea0871a2
Preet538-neitzen <sanghavipreet2001@gmail.com>
2020-04-23 16:32:46 +0530

    Updates

---
+++ precache-manifest.7b013fe64a551a591057c072d9fe1b40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caa6e605acda05ec673798877e6f65fa",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "a2af87887df8064c3a50",
+    "url": "/Covid-Tracker/static/css/main.edd9f97c.chunk.css"
+  },
+  {
+    "revision": "19baca82f82eab134e25",
+    "url": "/Covid-Tracker/static/js/2.398fa186.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-Tracker/static/js/2.398fa186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2af87887df8064c3a50",
+    "url": "/Covid-Tracker/static/js/main.fce00cc6.chunk.js"
+  },
+  {
+    "revision": "2def5a1351025b4d8753",
+    "url": "/Covid-Tracker/static/js/runtime-main.542baa7e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-Tracker/static/media/image.d7265326.png"
+  }
+]);
